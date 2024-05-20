190ec0d442abbe35d71455dc5f18ac1c266e05a2
Bob Bobson <44064260+bbobson@users.noreply.github.com>
2024-05-20 01:06:44 +0000

    .

--- src/_02_colors/VennDiagram/VennDiagramOfMe.java
+++ src/_02_colors/VennDiagram/VennDiagramOfMe.java
@@ -4,6 +4,13 @@ import processing.core.PApplet;
 
 public class VennDiagramOfMe extends PApplet {
 
+    @Override
+    public void draw() {
+        background(255); // Ensuring background is white to see the drawings
+
+        // Add your code here. 
+    }
+
     public static void main(String[] args) {
         VennDiagramOfMe vdom = new VennDiagramOfMe();
         //PApplet.main("_02_colors.VennDiagram.VennDiagramOfMe");
@@ -21,25 +28,5 @@ public class VennDiagramOfMe extends PApplet {
         surface.setResizable(true); // Allow resizing to help with display issues
         surface.setLocation(10, 10); // Set a specific location for the window
    
-    }
-
-    @Override
-    public void draw() {
-        background(255); // Ensuring background is white to see the drawings
-
-        fill(0, 250, 0, 100);
-        ellipse(350, 250, 200, 200);
-        
-        fill(250, 0, 0, 100);
-        ellipse(200, 250, 200, 200);
-
-        fill(0, 0, 250, 100);
-        ellipse(275, 350, 200, 200);
-
-        fill(0, 0, 0, 100);
-        textSize(25);
-        text("angry", 150, 200);
-        text("happy", 325, 200);
-        text("sad", 250, 375);
     }
 }
